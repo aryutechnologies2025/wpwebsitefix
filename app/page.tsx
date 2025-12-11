8ea7ac06ccf8972aa10864031cd05295ee5177f8
abdulrahman-mm <abdulrahman.mm124@gmail.com>
2025-12-11 12:31:40 +0530

    turbopack disabled

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const Home_Hero = dynamic(() => import("./components/home/Home_Hero"));
 const Home_About_Section = dynamic(
@@ -22,7 +24,46 @@ const Home_Testimonial = dynamic(
 const Home_CTA = dynamic(() => import("./components/home/Home_CTA"));
 
 export default function Home() {
+
+  // const[loading,setLoading]=useState(true)
+
+
+  // useEffect(() => {
+  //     // GSAP animation for preloader
+  //     const timeline = gsap.timeline({
+  //       onComplete: () => setLoading(false), // Hide preloader after animation
+  //     });
+  
+  //     timeline
+  //       .fromTo(
+  //         ".preloader-logo",
+  //         {
+  //           opacity: 0,
+  //           scale: 0.5, // Start smaller
+  //         },
+  //         {
+  //           duration: 0.5, //
+  //           opacity: 1,
+  //           scale: 2, // Slightly enlarge first
+  //           ease: "back.in",
+  //         }
+  //       )
+  //       .to(".preloader-logo", {
+  //         duration: 1,
+  //         scale: 20, // Expand to a very large size
+  //         opacity: 0, // Fade out smoothly
+  //         ease: "power4.in",
+  //       });
+  
+  //     // Cleanup timeline when component unmounts
+  //     return () => {
+  //       timeline.kill();
+  //     };
+  //   }, []);
+
+
   return (
+    
     <main>
       <Home_Hero />
       <Home_About_Section />
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    // Prevent Hostinger from caching old chunks
+    if (!dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
